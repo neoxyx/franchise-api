a1cd8ea50e743f17ea9fbc7a9e250755127522ec
Jhon <jhon.valdes@softgic.co>
2024-12-07 11:33:17 -0500

    Ajuste

--- src/main/java/com/example/franchiseapi/model/Franchise.java
+++ src/main/java/com/example/franchiseapi/model/Franchise.java
@@ -1,7 +1,4 @@
 package com.example.franchiseapi.model;
-
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -9,15 +6,7 @@ import org.springframework.data.relational.core.mapping.Table;
 public class Franchise {
     @Id
     private Long id;
-
     private String name;
-
-    // Una franquicia puede tener varias sucursales (relación 1:N)
-    private List<Branch> branches;
-
-    // Si tienes productos asociados directamente con la franquicia, también podrías
-    // incluir una lista de productos
-    private List<Product> products; // Lista de productos asociados a la franquicia, si es necesario
 
     public String getName() {
         return name;
